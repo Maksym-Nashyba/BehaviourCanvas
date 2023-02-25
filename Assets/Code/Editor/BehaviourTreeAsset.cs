abb99254032c19dd07b347e365ed4310045cdd8c
WingedCryptid <yariksouth@gmail.com>
2023-02-25 20:24:35 +0200

    Added BehaviourTreeAsset

---
+++ Assets/Code/Editor/BehaviourTreeAsset.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Code.Editor
+{
+    [CreateAssetMenu(fileName = "BehaviourTreeAsset", menuName = "BehaviourTreeAsset", order = 0)]
+    public class BehaviourTreeAsset : ScriptableObject
+    {
+        public TextAsset BehaviourTreeXML => _behaviourTreeXML;
+        public TextAsset EditorTreeXML => _editorTreeXML;
+    
+        [SerializeField] private TextAsset _behaviourTreeXML;
+        [SerializeField] private TextAsset _editorTreeXML;
+
+        public void UpdateAsset(TextAsset behaviourTreeXML, TextAsset editorTreeXML)
+        {
+            _behaviourTreeXML = behaviourTreeXML;
+            _editorTreeXML = editorTreeXML;
+        }
+    }
+}
