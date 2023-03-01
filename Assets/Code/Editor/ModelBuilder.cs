68b60459652f369bf12c4301ef8a0791163add82
WingedCryptid <yariksouth@gmail.com>
2023-03-01 19:48:24 +0200

    Refactored ModelBuilder

--- Assets/Code/Editor/ModelBuilder.cs
+++ Assets/Code/Editor/ModelBuilder.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Code.Editor.EditorWindows.Builders.StateBuilder;
 using Code.Editor.EditorWindows.Builders.TriggerBuilder;
 using Code.Runtime;
@@ -9,19 +11,22 @@ namespace Code.Editor
 {
     public class ModelBuilder : VisualElement
     {
-        private Button _statesSectionButton;
-        private Button _triggersSectionButton;
-
-        private VisualElement _statesVisual;
-        private ScrollView _statesScrollView;
-        private Button _createStateButton;
+        #region VisualElements
+            private Button _statesSectionButton;
+            private Button _triggersSectionButton;
         
-        private VisualElement _triggersVisual;
-        private ScrollView _triggersScrollView;
-        private Button _createTriggerButton;
-
+            private VisualElement _statesVisual;
+            private ScrollView _statesScrollView;
+            private Button _createStateButton;
+                
+            private VisualElement _triggersVisual;
+            private ScrollView _triggersScrollView;
+            private Button _createTriggerButton;
+        #endregion
+        
         private BehaviourCanvas _canvas;
         private BehaviourCanvasView _canvasView;
+        private readonly Rect _defaultNodePosition = new Rect(100, 100, 200, 100);
 
         public new class UxmlFactory : UxmlFactory<ModelBuilder> { }
 
@@ -34,17 +39,7 @@ namespace Code.Editor
         {
             _canvas = canvas;
             _canvasView = canvasView;
-            
-            _statesSectionButton = this.Q<Button>("states-section-button");
-            _triggersSectionButton = this.Q<Button>("triggers-section-button");
-            
-            _statesVisual = this.Q<VisualElement>("states-visual");
-            _statesScrollView = _statesVisual.Q<ScrollView>();
-            _createStateButton = _statesVisual.Q<Button>();
-            
-            _triggersVisual = this.Q<VisualElement>("triggers-visual");
-            _triggersScrollView = _triggersVisual.Q<ScrollView>();
-            _createTriggerButton = _triggersVisual.Q<Button>();
+            QueryAllVisualElements();
 
             UpdateStatesList();
             UpdateTriggersList();
@@ -55,7 +50,14 @@ namespace Code.Editor
         {
             _statesScrollView.Clear();
             IReadOnlyList<Model> models = Reflection.FindAllStates();
-            List<Button> buttons = CreateStatesListContentButtons(models);
+            IReadOnlyList<StateModel> states = models.Select(model => new StateModel(1, model)).ToList(); //TODO create IdStore
+            
+            List<Button> buttons = CreateTreeModelsButtons(states, treeModel =>
+            {
+                _canvas.AddState(treeModel as StateModel);
+                _canvasView.CreateNode(treeModel as StateModel, _defaultNodePosition);
+            });
+            
             foreach (Button button in buttons)
             {
                 _statesScrollView.Add(button);
@@ -64,49 +66,46 @@ namespace Code.Editor
         
         private void UpdateTriggersList()
         {
-            _triggersScrollView.Clear();
+            _statesScrollView.Clear();
             IReadOnlyList<Model> models = Reflection.FindAllTriggers();
-            List<Button> buttons = CreateTriggersListContentButtons(models);
+            IReadOnlyList<TriggerModel> triggers = models.Select(model => new TriggerModel(1, model, false)).ToList(); //TODO create IdStore
+            
+            List<Button> buttons = CreateTreeModelsButtons(triggers, treeModel =>
+            {
+                _canvas.AddTrigger(treeModel as TriggerModel);
+                _canvasView.CreateTriggerNode(treeModel as TriggerModel, _defaultNodePosition);
+            });
+            
             foreach (Button button in buttons)
             {
                 _triggersScrollView.Add(button);
             }
         }
 
-        private List<Button> CreateStatesListContentButtons(IReadOnlyList<Model> models)
+        private List<Button> CreateTreeModelsButtons(IReadOnlyList<TreeModel> treeModels, Action<TreeModel> buttonClickEvent)
         {
-            List<Button> buttons = new List<Button>(models.Count);
-            foreach (Model model in models)
+            List<Button> buttons = new List<Button>(treeModels.Count);
+            foreach (TreeModel treeModel in treeModels)
             {
-                Button button = new Button(() =>
-                {
-                    StateModel state = new StateModel(1, model);
-                    _canvas.AddState(state);
-                    _canvasView.CreateNode(state, new Rect(100, 100, 200, 100)); //TODO create IdStore
-                });
-                button.text = model.Name;
+                Button button = new Button(() => buttonClickEvent(treeModel));
+                button.text = treeModel.Model.Name;
                 buttons.Add(button);
             }
-
             return buttons;
         }
         
-        private List<Button> CreateTriggersListContentButtons(IReadOnlyList<Model> models)
+        private void QueryAllVisualElements()
         {
-            List<Button> buttons = new List<Button>(models.Count);
-            foreach (Model model in models)
-            {
-                Button button = new Button(() => //TODO method with TreeModel parameters
-                {
-                    TriggerModel trigger = new TriggerModel(2, model, false);
-                    _canvas.AddTrigger(trigger);
-                    _canvasView.CreateTriggerNode(trigger, new Rect(100, 100, 200, 100)); //TODO create IdStore
-                });
-                button.text = model.Name;
-                buttons.Add(button);
-            }
-
-            return buttons;
+            _statesSectionButton = this.Q<Button>("states-section-button");
+            _triggersSectionButton = this.Q<Button>("triggers-section-button");
+            
+            _statesVisual = this.Q<VisualElement>("states-visual");
+            _statesScrollView = _statesVisual.Q<ScrollView>();
+            _createStateButton = _statesVisual.Q<Button>();
+            
+            _triggersVisual = this.Q<VisualElement>("triggers-visual");
+            _triggersScrollView = _triggersVisual.Q<ScrollView>();
+            _createTriggerButton = _triggersVisual.Q<Button>();
         }
 
         private void SubscribeButtons()
