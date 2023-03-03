8ad857635c1b147083ae85fce8f57df51848284a
WingedCryptid <yariksouth@gmail.com>
2023-03-03 17:03:06 +0200

    Added id store

---
+++ Assets/Code/Editor/IdStore.cs
@@ -0,0 +1,13 @@
+﻿namespace Code.Editor
+{
+    public class IdStore
+    {
+        public int ID => _id++;
+        private int _id;
+        
+        public IdStore()
+        {
+            _id = 1;
+        }
+    }
+}
