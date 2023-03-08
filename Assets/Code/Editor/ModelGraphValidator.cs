ad29b892b6f75d55256042bae96e060e666c45cf
Maksym-Nashyba <94983728+Maksym-Nashyba@users.noreply.github.com>
2023-03-08 13:11:17 +0100

    Update ModelGraphValidator.cs

--- Assets/Code/Editor/ModelGraphValidator.cs
+++ Assets/Code/Editor/ModelGraphValidator.cs
@@ -11,7 +11,7 @@ namespace Code.Editor
         public static void Validate(IReadOnlyModelGraph graph)
         {
             ValidateStatesHaveSource(graph);
-            ValidateTriggersHaveSource(graph);
+            ValidateTriggersHaveOneSource(graph);
             ValidateTriggersHaveTargets(graph);
             ValidateTriggerToStateParameters(graph);
         }
@@ -48,24 +48,32 @@ namespace Code.Editor
             }
         }
         
-        private static void ValidateTriggersHaveSource(IReadOnlyModelGraph graph)
+        private static void ValidateTriggersHaveOneSource(IReadOnlyModelGraph graph)
         {
             Dictionary<IReadOnlyTriggerModel, IReadOnlyBehaviourElementModel> triggerToSource =
                 new Dictionary<IReadOnlyTriggerModel, IReadOnlyBehaviourElementModel>();
             foreach (IReadOnlyBehaviourElementModel state in graph.GetStates().Values)
             {
-                foreach (IReadOnlyBehaviourElementModel targetTriggers in state.GetTargetModels())
+                foreach (IReadOnlyTriggerModel targetTrigger in state.GetTargetModels().Cast<IReadOnlyTriggerModel>())
                 {
-                    
+                    if (triggerToSource.ContainsKey(targetTrigger))
+                    {
+                        IReadOnlyBehaviourElementModel existingSource = triggerToSource[targetTrigger];
+                        IReadOnlyBehaviourElementModel newSource = state;
+                        throw new InvalidDataException($"Trigger {targetTrigger.GetModel().Name}_{targetTrigger.GetId()}" +
+                                                       $" has more than one source: {existingSource.GetId()} and {newSource.GetId()}");
+                    }
+                    triggerToSource.Add(targetTrigger, state);
                 }
             }
+
+            bool allTriggersAdded = graph.GetTriggers().Values.Intersect(triggerToSource.Keys).Count() == triggerToSource.Keys.Count;
+            if (!allTriggersAdded) throw new InvalidDataException("Some triggers have no source");
         }
         
         private static void ValidateStatesHaveSource(IReadOnlyModelGraph graph)
         {
             throw new NotImplementedException();
         }
-        
-
     }
 }
